a083695ef798537445f0e8cd5c2b7d4c0d2aa226
Cameron Ashley <cameron@ashbi.ca>
2026-04-24 10:37:54 -0400

    fix: US footer - logo now inside white card matching CA layout

--- themes/harrisexploration/footer.php
+++ themes/harrisexploration/footer.php
@@ -13,43 +13,59 @@ $addresses = Hny_Site_Settings::get_addresses();
 		<div class="site-footer__contact">
 			<div class="site-footer__background"></div>
 			<div class="site-footer__content">
-				<div class="site-footer__heading">
-					<div class="grid-container">
-						<?php
-						hny_get_template_part(
-							'partials/heading-group',
-							array(
-								'heading' => 'Contact ',
-								'level'   => 4,
-								'drill'   => true,
-							)
-						);
-						?>
-					</div>
-				</div>
 				<div class="site-footer__columns">
-					<div class="grid-container">
+					<div class="grid-container site-footer__card">
 						<div class="grid-x grid-padding-x">
 							<div class="small-12 large-10 xlarge-9 cell">
 								<div class="grid-x grid-padding-x grid-padding-x--small">
 									<div class="small-12 large-4 large-shrink cell">
 										<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"
-												title="<?php echo get_bloginfo( 'name' ); ?>" class="site-footer__logo">
+												title="<?php bloginfo( 'name' ); ?>" class="site-footer__logo">
 											<div class="u-svg-container u-svg-container--footer-logo">
 												<img src="<?php echo hny_get_theme_image( 'logo-foot.png' ); ?>"
 														alt="Earth Drilling" />
 											</div>
-											<span class="u-screen-reader"><?php echo get_bloginfo( 'name' ); ?> - Return to home page</span>
+											<span class="u-screen-reader"><?php bloginfo( 'name' ); ?> - Return to home page</span>
 										</a>
 									</div>
-									<?php
-									hny_get_template_part( 'partials/address');
-									?>
 									<div class="small-12 large-auto cell">
-										<?php hny_get_template_part(
-											'partials/associations',
-										);
-										?>
+										<p class="site-footer__intro">Contact the Harris Exploration Office nearest to you</p>
+										<div class="grid-x grid-padding-x grid-padding-x--small">
+											<?php
+											foreach ( $addresses as $address ) {
+												$phone = $address['phone'];
+												?>
+												<div class="small-6 large-auto large-shrink cell">
+													<?php
+													hny_get_template_part(
+														'partials/heading-group',
+														array(
+															'heading' => $address['city'],
+															'level'   => 5,
+														)
+													);
+													?>
+													<address>
+														<?php
+														echo ( $address['address'] ? $address['address'] . '<br>' : '' ) . $address['city'] . ', ' . $address['province_state'] . ' ' . $address['zip_postal_code'];
+														?>
+														<?php
+														if ( $phone ) {
+															?>
+															<br />
+															<a href="tel:<?php echo hny_to_tel( $phone ); ?>"
+																	class="inline-icon">
+																<?php echo hny_get_svg( array( 'icon' => 'phone' ) ); ?>
+																<span><?php echo $phone; ?></span></a>
+															<?php
+														}
+														?>
+													</address>
+												</div>
+												<?php
+											}
+											?>
+										</div>
 									</div>
 								</div>
 							</div>
